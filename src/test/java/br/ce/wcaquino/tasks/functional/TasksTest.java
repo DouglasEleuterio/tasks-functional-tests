bcc09d19e2da6ac1fde792eae8421cf63316faa5
Douglas <douglasferreira@hdregistros.com.br>
2021-08-08 15:29:37 -0300

    Alterações para rodar com Docker.

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -5,23 +5,29 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class TasksTest {
 
-    private WebDriver acessarAplicacao(){
+    private WebDriver acessarAplicacao() throws MalformedURLException {
         System.setProperty("webdriver.chrome.driver", "D:\\C\\devops\\chromedriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://localhost:8001/tasks");
+        //WebDriver driver = new ChromeDriver();
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.104:4444/wd/hub"), cap);
+        driver.navigate().to("http://192.168.0.104:8001/tasks");
         return driver;
     }
 
     @Test
-    public void deveSalvarTarefaComSucesso(){
+    public void deveSalvarTarefaComSucesso() throws MalformedURLException {
         WebDriver driver = acessarAplicacao();
         try {
-            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); //Aguardando 10 Segundos para disparar a ação.
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); //Aguardando até 3 Segundos para disparar a ação.
             //Clicar em Add Todo
             driver.findElement(By.id("addTodo")).click();
 
@@ -44,7 +50,7 @@ public class TasksTest {
     }
 
     @Test
-    public void naoDeveSalvarTarefaSemDescricao(){
+    public void naoDeveSalvarTarefaSemDescricao() throws MalformedURLException {
         WebDriver driver = acessarAplicacao();
         try {
             //Clicar em Add Todo
@@ -66,7 +72,7 @@ public class TasksTest {
     }
 
     @Test
-    public void naoDeveSalvarTarefaSemData(){
+    public void naoDeveSalvarTarefaSemData() throws MalformedURLException {
         WebDriver driver = acessarAplicacao();
         try {
             //Clicar em Add Todo
@@ -88,7 +94,7 @@ public class TasksTest {
     }
 
     @Test
-    public void naoDeveSalvarTarefaComDataPassada(){
+    public void naoDeveSalvarTarefaComDataPassada() throws MalformedURLException {
         WebDriver driver = acessarAplicacao();
         try {
             driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); //Aguardando 10 Segundos para disparar a ação.
